34170afce1c10baa534acef7bd63a1bfc5453ea8
vandermjr <50533285+vandermjr@users.noreply.github.com>
2020-11-20 21:55:44 -0400

    Update ClassTraduz.cs

--- Tradutor Ws - V3.1/ClassTraduz.cs
+++ Tradutor Ws - V3.1/ClassTraduz.cs
@@ -12,30 +12,23 @@ using System.ComponentModel;
 namespace Tradutor_Ws___V3._1
 {
     public class ClassTraduz
-    {
-        public static TextIdiomaDetect { get; set; }
-        public static TextIdSaida { get; set; }
-        
+    {        
         private static readonly string subscriptionKey = "b38bee5190mshb800f582c906e34p1004efjsn1d9fef204f63";
         private static readonly string endpoint = "https://microsoft-translator-text.p.rapidapi.com/";
         private static readonly string location = "AWS - ap-southeast-1";
-
-        public static void TraduzClic(string ie, string is)
+        
+        // ie = idioma entrada (from), is = idioma saida (to), te = texto entrada (from)
+        public static TradutorSaida TraduzClic(string ie, string is, string te)
         {
             FrmLoanding FrmL = new FrmLoanding();
             FrmL.Show();
 
-            var Ie = Program.form1.TextIdiomaDetect.Text; //Idioma de entrada
-            var Is = Program.form1.TextIdSaida.Text; //Idioma de saida
+            var route = "translate?to=" + is + "&api-version=3.0&from=" + ie + "&profanityAction=NoAction&textType=plain";
 
-            var route = "translate?to=" + Is + "&api-version=3.0&from=" + Ie + "&profanityAction=NoAction&textType=plain";
-
-            var textToTranslate = Program.form1.TextFrom.Text;
-            var body = new object[] { new { Text = textToTranslate } };
+            var body = new object[] { new { Text = te } };
             var requestBody = JsonConvert.SerializeObject(body);
             var client = new HttpClient();
             var request = new HttpRequestMessage();
-
 
             request.Method = HttpMethod.Post;
             request.RequestUri = new Uri(endpoint + route);
@@ -43,11 +36,9 @@ namespace Tradutor_Ws___V3._1
             request.Headers.Add("x-rapidapi-key", subscriptionKey);
             request.Headers.Add("x-rapidapi-Region", location);
 
-
             HttpResponseMessage thing = client.SendAsync(request).Result;
             string actualResponse = thing.Content.ReadAsStringAsync().Result;
-
-
+            
             var Saida = JsonConvert.DeserializeObject(actualResponse);
             string S = Saida.ToString();
             S = S.Remove(0, 48);
@@ -56,10 +47,7 @@ namespace Tradutor_Ws___V3._1
 
             string json = @"{" + S + "}";
 
-            TradutorSaida tradu = JsonConvert.DeserializeObject<TradutorSaida>(json);
-            string name = tradu.text;
-
-            Program.form1.TextTo.Text = name;
+            return JsonConvert.DeserializeObject<TradutorSaida>(json);
 
             FrmL.Close();
         }
